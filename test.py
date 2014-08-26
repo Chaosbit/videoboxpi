dcee1b46d12cc27977418fc02d41ea2eecf2c93e
root <root@holzkiste.(none)>
2014-08-26 19:31:34 +0000

    improved logging

--- test.py
+++ test.py
@@ -3,50 +3,56 @@
 import RPi.GPIO as GPIO
 import signal
 from pyomxplayer import OMXPlayer
+from logbook import Logger
+from logbook import SyslogHandler
 
+log = Logger('Door Logger')
+error_handler = SyslogHandler('logbook example', level='DEBUG')
+
+log.debug('setting up GPIO pin 23...')  
 GPIO.setmode(GPIO.BCM)  
-  
-GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+log.debug('done.')
 
+log.debug('Setting up the omxplayer instance...')
 omx_status = False 
 omx = OMXPlayer('/videos/dgzrs.mp4', '-o both -rb')
+log.debug('done.')
 
 def start_video():
     global omx, omx_status
     if(omx_status):
-	print "video already running"
+	log.warn('video already running')
     else:
 	omx.toggle_pause()
 	omx_status = True
+	log.info('door opened')
 
 def stop_video():
     global omx, omx_status
     if(omx_status):
-    	#omx.stop()
-    	#omx = OMXPlayer('/videos/dgzrs.mp4', '-o local -rb')
 	omx.toggle_pause()
 	omx.previous_chapter()
 	omx_status = False
+	log.info('door closed')
     else:
-	print "video not running"
+	log.warn('video not running')
   
 def my_callback2(channel):
     if GPIO.input(channel) > 0:
-    	print "stopping video"
 	stop_video()
     else:
-        print "starting video"
 	start_video()
   
 GPIO.add_event_detect(23, GPIO.BOTH, callback=my_callback2, bouncetime=100)  
   
 try:  
-    print "Pausing and waiting for interrupt..."
+    log.info('Pausing and waiting for interrupt...')
     signal.pause()
-    print "Something went wrong..."  
+    log.debug('Something went wrong...')  
   
 except KeyboardInterrupt:  
-    GPIO.cleanup()       # clean up GPIO on CTRL+C exit
+    GPIO.cleanup()
     omx.stop()
-GPIO.cleanup()           # clean up GPIO on normal exit  
+GPIO.cleanup()
 omx.stop()
